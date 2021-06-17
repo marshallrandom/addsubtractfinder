b17a7671176a0c4c285fdb0c250778d87fe54c65
marshallrandom <81017485+marshallrandom@users.noreply.github.com>
2021-06-16 19:07:59 -0500

    main project files

---
+++ Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace problem3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.process1 = new System.Diagnostics.Process();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 17);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(66, 26);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(117, 17);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(66, 26);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "2";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(212, 17);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(66, 26);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Text = "3";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(309, 17);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(66, 26);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.Text = "4";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(408, 17);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(66, 26);
+            this.textBox5.TabIndex = 4;
+            this.textBox5.Text = "5";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(504, 17);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(66, 26);
+            this.textBox6.TabIndex = 5;
+            this.textBox6.Text = "6";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(600, 17);
+            this.textBox7.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(66, 26);
+            this.textBox7.TabIndex = 6;
+            this.textBox7.Text = "7";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(696, 17);
+            this.textBox8.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(66, 26);
+            this.textBox8.TabIndex = 7;
+            this.textBox8.Text = "8";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(789, 17);
+            this.textBox9.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(66, 26);
+            this.textBox9.TabIndex = 8;
+            this.textBox9.Text = "9";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(867, 20);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 32);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "=";
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(908, 17);
+            this.textBox10.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(66, 26);
+            this.textBox10.TabIndex = 10;
+            this.textBox10.Text = "100";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(20, 74);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 40);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Compute";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(22, 138);
+            this.textBox11.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox11.Multiline = true;
+            this.textBox11.Name = "textBox11";
+            this.textBox11.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox11.Size = new System.Drawing.Size(758, 342);
+            this.textBox11.TabIndex = 12;
+            // 
+            // process1
+            // 
+            this.process1.StartInfo.Domain = "";
+            this.process1.StartInfo.LoadUserProfile = false;
+            this.process1.StartInfo.Password = null;
+            this.process1.StartInfo.StandardErrorEncoding = null;
+            this.process1.StartInfo.StandardOutputEncoding = null;
+            this.process1.StartInfo.UserName = "";
+            this.process1.SynchronizingObject = this;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(180, 77);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.progressBar1.Maximum = 6561;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(530, 35);
+            this.progressBar1.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1030, 518);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Add/Subtract Answer Finder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Diagnostics.Process process1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NCalc;
+
+namespace problem3
+{
+
+    public partial class Form1 : Form
+    {
+        int[] numbers;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] operations;
+            progressBar1.Value = 0;
+            numbers = new int[9];  // numbers is a 10-element array
+            operations = new int[8]  { 0, 0, 0, 0, 0, 0, 0, 0 };
+            numbers[0] = Convert.ToInt32(textBox1.Text);
+            numbers[1] = Convert.ToInt32(textBox2.Text);
+            numbers[2] = Convert.ToInt32(textBox3.Text);
+            numbers[3] = Convert.ToInt32(textBox4.Text);
+            numbers[4] = Convert.ToInt32(textBox5.Text);
+            numbers[5] = Convert.ToInt32(textBox6.Text);
+            numbers[6] = Convert.ToInt32(textBox7.Text);
+            numbers[7] = Convert.ToInt32(textBox8.Text);
+            numbers[8] = Convert.ToInt32(textBox9.Text);
+           // numbers[9] = Convert.ToInt32(textBox10.Text);
+            //numbers[10] = Convert.ToInt32(textBox11.Text);
+            textBox11.Text = "";
+            findeveryone(operations, 0);
+
+
+
+
+        }
+        private void findeveryone(int[] operations, int placenumber)
+        {
+            Expression loexp = new Expression("2 + 3 * 5");
+            string expressionstring;
+            
+            if (placenumber == 7)
+            {
+                operations[7] = 0;
+                expressionstring = exptostring(operations);
+                loexp = new Expression(expressionstring);
+                progressBar1.Value++;
+                if (Convert.ToInt32(loexp.Evaluate()) == Convert.ToInt32(textBox10.Text))
+                {
+                    textBox11.Text += expressionstring + "=" + loexp.Evaluate() + Environment.NewLine;
+                }
+
+                operations[7] = 1;
+                expressionstring = exptostring(operations);
+                loexp = new Expression(expressionstring);
+                progressBar1.Value++;
+                if (Convert.ToInt32(loexp.Evaluate()) == Convert.ToInt32(textBox10.Text))
+                {
+                    textBox11.Text += expressionstring + "=" + loexp.Evaluate() + Environment.NewLine;
+                }
+
+                operations[7] = 2;
+                expressionstring = exptostring(operations);
+                loexp = new Expression(expressionstring);
+                progressBar1.Value++;
+                if (Convert.ToInt32(loexp.Evaluate()) == Convert.ToInt32(textBox10.Text))
+                {
+                    textBox11.Text += expressionstring + "=" + loexp.Evaluate() + Environment.NewLine;
+                }
+
+            }
+            else
+            {
+                operations[placenumber] = 0;
+                findeveryone(operations, placenumber + 1);
+                operations[placenumber] = 1;
+                findeveryone(operations, placenumber + 1);
+                operations[placenumber] = 2;
+                findeveryone(operations, placenumber + 1);
+
+            }
+            
+
+
+        }
+        private string exptostring( int[] operations)
+        {
+            string temp_string = "";
+            for (int i = 0; i <= numbers.GetUpperBound(0); i++)
+            {
+                temp_string += Convert.ToString(numbers[i]);
+                if (i < numbers.GetUpperBound(0))
+                {
+                    if (operations[i] == 0)
+                        temp_string += "+";
+                    else if (operations[i] == 1)
+                        temp_string += "-";
+                   
+
+
+                }
+
+            }
+            return temp_string;
+
+
+        }
+
+        
+    }
+}
